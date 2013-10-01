a7f310595e62ffa80c48ec95de1f209e252ffe8b
Yevgen <godun.yevgen@gmail.com>
2013-10-01 10:25:51 +0300

    home.html DONE

--- js/common.js
+++ js/common.js
@@ -1,5 +1,39 @@
 $(document).ready(function() {
-    $('.scroll-pane').jScrollPane({
-        autoReinitialise: true
-    });
-});
+    if ($('.scroll-pane').length > 0) {
+        $('.scroll-pane').jScrollPane({
+            autoReinitialise: true
+        });
+    }
+    // $('#cycle-1').cycle();
+    // $('#cycle-2').cycle();
+    function init_cycle() {
+        if ($(".js-slider").length > 0) {
+
+            $(".js-slider").each(function(){
+                var slider_1 = $(this).find('.js-cycle-1');
+                var slider_2 = $(this).find('.js-cycle-2');
+
+                slider_1.cycle({
+                    prev: ".js-cycle-prev",
+                    next: ".js-cycle-next"
+                });
+                slider_2.cycle();
+
+                var slideshows = $(this).find('.js-cycle').on('cycle-next cycle-prev', function(e, opts) {
+                    // advance the other slideshow
+                    slideshows.not(this).cycle('goto', opts.currSlide);
+                });
+
+                slider_2.find(".cycle-slide").click(function(){
+                    var index = slider_2.data('cycle.API').getSlideIndex(this);
+                    slideshows.cycle('goto', index);
+                });
+            });
+        }
+    }
+    init_cycle();
+    
+
+    
+    
+}); 
